d281da5d8614ce838cb9d4e4e5793cfd14925e13
Tatsuya Kai <tatsuya.develop@gmail.com>
2024-08-04 21:11:13 +0900

    add: React初期導入

---
+++ react/vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // root: 'src',
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  build: {
+    // distフォルダに出力
+    outDir: resolve(__dirname, 'dist'),
+    // 存在しないときはフォルダを作成する
+    emptyOutDir: true,
+    rollupOptions: {
+      // entry-pointがあるindex.htmlのパス
+      input: {
+        '': resolve(__dirname, 'index.html'),
+      },
+      // bundle.jsを差し替えする
+      output: {
+        entryFileNames: 'assets/[name]/bundle.js',
+      },
+    },
+  },
+});
